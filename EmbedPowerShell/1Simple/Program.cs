e92b5c61e428fa1077ada51dfb038ab2d88fec38
Doug Finke <finked@hotmail.com>
2012-09-19 19:36:20 -0400

    Simple embedding of PowerShell

---
+++ EmbedPowerShell/1Simple/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var powerShellResult = PowerShell
+                .Create()
+                .AddScript("Get-ChildItem")                
+                .Invoke();
+
+            foreach (var item in powerShellResult)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
