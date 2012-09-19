4da60625db2931da5c808a9018abad96c587666c
Doug Finke <finked@hotmail.com>
2012-09-19 19:43:27 -0400

    Embeds PowerShell in a console app
    Lets you pass in PS commands and executes them

---
+++ EmbedPowerShell/2SimpleWithFormatting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace SimpleWithFormatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Adds the command Out-String
+            // Works with args
+
+            var script = "Get-ChildItem";
+            if (args.Length > 0)
+            {
+                script = args[0];
+            }
+
+            var powerShellResult = PowerShell
+                .Create()
+                .AddScript(script)
+                .AddCommand("Out-String")
+                .Invoke();
+
+            foreach (var item in powerShellResult)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
