1bfbb6021f391b52aeaca34940473cd16b9fc4f3
Doug Finke <finked@hotmail.com>
2012-10-06 11:49:57 -0400

    Embedding PowerShell in a WPF GUI

---
+++ EmbedPowerShell/6TheGUI/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Windows;
+
+namespace _6TheGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public static class PowerShellExtensions
+    {
+        static Runspace rs;
+
+        static Runspace TheRunSpace
+        {
+            get
+            {
+                if (rs == null)
+                {
+                    rs = RunspaceFactory.CreateRunspace();
+                    rs.Open();
+                }
+
+                return rs;
+            }
+        }
+
+        public static void AddVariable(string name, object value)
+        {
+            TheRunSpace.SessionStateProxy.SetVariable(name, value);
+        }
+
+        public static PSObject ExecutePowerShell(this string script)
+        {
+            if (System.IO.File.Exists(script))
+            {
+                script = System.IO.File.ReadAllText(script);
+            }
+
+            var ps = PowerShell.Create();
+            ps.Runspace = TheRunSpace;
+
+            return ps
+                .AddScript(script)
+                .AddCommand("Out-String")
+                .Invoke()[0];
+        }
+
+        public static void Print(this PSObject target)
+        {
+            Console.WriteLine(target);
+        }
+    }
+
+}
